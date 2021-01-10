472f414d30dfd4a676aaafa15c2f685d291459e1
Apple <sdjee2015@gmail.com>
2021-01-10 15:53:51 +0530

    Tour project fetch and display data

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -1,11 +1,41 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const getData = async () => {
+    try {
+      const res = await fetch(url);
+      const jsonRes = await res.json();
+      setTours(jsonRes);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <Loading />
+      </>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  );
 }
 
-export default App
+export default App;
