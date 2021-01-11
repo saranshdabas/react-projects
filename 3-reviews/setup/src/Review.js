976974fc94f18fa691c508010798741aa11eb132
Apple <sdjee2015@gmail.com>
2021-01-11 22:41:42 +0530

    Implemented prev, next and random review feature

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -6,6 +6,29 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { id, name, job, image, text } = people[index];
 
+  const nextPerson = () => {
+    setIndex((index) => {
+      return (index + 1) % people.length;
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      return (people.length + index - 1) % people.length;
+    });
+  };
+
+  const randomPerson = () => {
+    setIndex((index) => {
+      let randomNumber = Math.random() * people.length;
+      randomNumber = Math.floor(randomNumber);
+      if (randomNumber == index) {
+        randomNumber += 1;
+      }
+      return randomNumber % people.length;
+    });
+  };
+
   return (
     <article className="review">
       <div className="img-container">
@@ -17,6 +40,17 @@ const Review = () => {
       <h4 className="author">{name}</h4>
       <p className="job">{job}</p>
       <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn" onClick={randomPerson}>
+        surprise me
+      </button>
     </article>
   );
 };
