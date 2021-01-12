afadf1eb3169714b4a26e3b89328e2b687690521
Apple <sdjee2015@gmail.com>
2021-01-12 23:37:23 +0530

    Accordion UI

--- 4-accordion/setup/src/App.js
+++ 4-accordion/setup/src/App.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <div className="container">
+        <h4>Questions</h4>
+        <section className="info">
+          {data.map((ques) => {
+            return <SingleQuestion id={ques.id} {...ques} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
