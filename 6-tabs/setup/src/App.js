0fba5ec7029904680097e8b4d0f8b66491ce431e
Apple <sdjee2015@gmail.com>
2021-01-13 23:09:15 +0530

    Fetching data - tabs project

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -1,10 +1,29 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
 function App() {
-  return <h2>tabs project setup</h2>
+  const [jobs, setJobs] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const fecthJobs = async () => {
+    const res = await fetch(url);
+    const resJson = await res.json();
+    setJobs(resJson);
+    setLoading(false);
+  };
+
+  useEffect(() => fecthJobs(), []);
+
+  if (loading) {
+    return (
+      <div className="loader">
+        <h4>Loading...</h4>
+      </div>
+    );
+  }
+  return <h2>Jobs</h2>;
 }
 
-export default App
+export default App;
