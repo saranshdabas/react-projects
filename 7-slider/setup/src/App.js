0698bbb99fe19e8623a8f85f91baea78c39581ec
Apple <sdjee2015@gmail.com>
2021-01-14 15:44:34 +0530

    UI for slider

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -1,9 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+import people from "./data";
 function App() {
-  return <h2>slider project setup</h2>;
+  const [person, setPerson] = useState(data);
+  const [index, setIndex] = useState(data);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default App;
