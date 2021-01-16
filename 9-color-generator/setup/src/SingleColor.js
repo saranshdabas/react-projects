f0b5f19dce6d14da1a90d3b6569f4cbf3976730d
Apple <sdjee2015@gmail.com>
2021-01-16 23:28:09 +0530

    Copy to clipboard functionality for color-generator project

--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -2,14 +2,29 @@ import React, { useState, useEffect } from "react";
 import rgbToHex from "./utils";
 
 const SingleColor = ({ weight, hexValue, index }) => {
-  console.log(index);
+  const [alert, setAlert] = useState(false);
+
+  const handleClick = () => {
+    setAlert(true);
+    navigator.clipboard.writeText(hexValue);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
   return (
     <article
+      onClick={handleClick}
       className={`color ${index > 10 ? "color-light" : null}`}
       style={{ background: `#${hexValue}` }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">#{hexValue}</p>
+      {alert && <p className="alert">Copied to Clipboard</p>}
     </article>
   );
 };
