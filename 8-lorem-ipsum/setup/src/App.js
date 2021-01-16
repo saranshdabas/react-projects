da859f3921973884e03b0fc39a637be750d62d19
Apple <sdjee2015@gmail.com>
2021-01-16 20:52:41 +0530

    Functionality for lorem-ipsum project

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,45 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [paragraphs, setParagraphs] = useState([]);
+
+  const handleGenerate = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (amount <= 0) {
+      amount = 1;
+    }
+    if (amount > data.length) {
+      amount = data.length;
+    }
+    //Slice takes starting index and ending index not including ending
+    setParagraphs(data.slice(0, amount));
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum ?</h3>
+      <form className="lorem-form">
+        <label htmlFor="count">Paragraph : </label>
+        <input
+          type="text"
+          id="count"
+          name="count"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        ></input>
+        <button className="btn" onClick={handleGenerate}>
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {paragraphs.map((paragraph, index) => {
+          return <p key={index}>{paragraph}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
