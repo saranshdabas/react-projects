b9e8ae70b08a474667140c1c5e7baec0df93d7ae
Apple <sdjee2015@gmail.com>
2021-01-18 22:38:51 +0530

    Normal approach of mounting-unmounting component to view links using useState

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,27 +4,30 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
+  const [showList, setShowList] = useState(false);
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={() => setShowList(!showList)}>
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
-            {links.map((link) => {
-              const { id, url, text } = link;
-              return (
-                <li key={id}>
-                  <a href={url}>{text}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        {showList && (
+          <div className="links-container show-container">
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
         <ul className="social-icons">
           {social.map((link) => {
             const { id, url, icon } = link;
