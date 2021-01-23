eb98248292a8eb19b834d72d134f9532f22aef56
Apple <sdjee2015@gmail.com>
2021-01-23 18:43:28 +0530

    Sidebar component in stripe-submenus project

--- 13-stripe-submenus/setup/src/Sidebar.js
+++ 13-stripe-submenus/setup/src/Sidebar.js
@@ -1,9 +1,42 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "../../setup/src/context";
+import sublinks from "./data";
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
-}
+  const { sidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <div
+      className={`${sidebarOpen ? "sidebar-wrapper show" : "sidebar-wrapper"}`}
+    >
+      <aside className="sidebar">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((sublink, index) => {
+            const { page, links } = sublink;
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link, index) => {
+                    const { label, icon, url } = link;
+                    return (
+                      <a key={index} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </aside>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
