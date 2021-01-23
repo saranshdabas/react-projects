efd8c03cc1f8cd1d7d7bcb3e980806e956d5529f
Apple <sdjee2015@gmail.com>
2021-01-23 16:05:30 +0530

    Boilerplate code for stripe-submenus project

--- 13-stripe-submenus/setup/src/App.js
+++ 13-stripe-submenus/setup/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Hero from './Hero'
-import Sidebar from './Sidebar'
-import Submenu from './Submenu'
+import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+import { useGlobalContext } from "./context";
+
 function App() {
+  const data = useGlobalContext();
+  console.log(data);
   return (
     <>
-      <h2>stripe submenus setup</h2>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,41 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [submenuOpen, setSubmenuOpen] = useState(false);
+
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+  const openSubmenu = () => {
+    setSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        sidebarOpen,
+        submenuOpen,
+        openSidebar,
+        closeSidebar,
+        openSubmenu,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
