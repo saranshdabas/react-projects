f940865e078c53284cf68a60c1c50a03a7278619
Apple <sdjee2015@gmail.com>
2021-01-24 21:06:53 +0530

    Increase and decrease item fn for cart project

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -21,12 +21,20 @@ const AppProvider = ({ children }) => {
   const removeItem = (id) => {
     dispatch({ type: "REMOVE_ITEM", payload: id });
   };
+  const increaseItem = (id) => {
+    dispatch({ type: "INCREASE_ITEM", payload: id });
+  };
+  const decreaseItem = (id) => {
+    dispatch({ type: "DECREASE_ITEM", payload: id });
+  };
   return (
     <AppContext.Provider
       value={{
         ...state,
         clearCart,
         removeItem,
+        increaseItem,
+        decreaseItem,
       }}
     >
       {children}
--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -8,5 +8,31 @@ export const reducer = (state, action) => {
       cart: state.cart.filter((item) => item.id !== action.payload),
     };
   }
+  if (action.type === "INCREASE_ITEM") {
+    const newCart = state.cart.map((item) => {
+      if (item.id == action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return {
+      ...state,
+      cart: newCart,
+    };
+  }
+  if (action.type === "DECREASE_ITEM") {
+    const newCart = state.cart
+      .map((item) => {
+        if (item.id == action.payload) {
+          return { ...item, amount: item.amount - 1 };
+        }
+        return item;
+      })
+      .filter((item) => item.amount !== 0);
+    return {
+      ...state,
+      cart: newCart,
+    };
+  }
   return state;
 };
