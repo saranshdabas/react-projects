10a3999aec38e9b16b6bc2deef40ccb94fde2975
Apple <sdjee2015@gmail.com>
2021-01-25 23:25:27 +0530

    Error and about pages for project cocktail

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione hic
+        dicta similique harum iure consequatur eligendi officiis dignissimos
+        numquam quod cumque fugiat, quidem id doloribus porro? Nesciunt, laborum
+        magnam. Harum eum fugiat cum repellat perspiciatis iusto similique
+        libero quae officiis?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
