e56065da9525702f0bcc32406758127dcee8b933
Apple <sdjee2015@gmail.com>
2021-01-26 22:50:42 +0530

    Fetch cocktail from API for project cocktail

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -6,8 +6,30 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
-  const [searchCocktail, setSearchCocktail] = useState("");
+  const [searchCocktail, setSearchCocktail] = useState("a");
   const [cocktails, setCocktails] = useState([]);
+
+  const fetchCocktails = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(`${url}${searchCocktail}`);
+      const data = await res.json();
+      const { drinks } = data;
+      if (drinks) {
+        setCocktails(drinks);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCocktails();
+  }, [searchCocktail]);
   return (
     <AppContext.Provider value={{ loading, setSearchCocktail, cocktails }}>
       {children}
