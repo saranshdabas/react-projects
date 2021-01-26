c2eddc7aa76269843a97cbed366bd7b811131c29
Apple <sdjee2015@gmail.com>
2021-01-27 00:24:15 +0530

    Cocktail search fn for project cocktail

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -3,11 +3,26 @@ import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
   const { setSearchCocktail } = useGlobalContext();
-
+  const searchValue = React.useRef("");
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleValueChange = () => {
+    const value = searchValue.current.value;
+    setSearchCocktail(value);
+  };
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form onSubmit={handleSubmit} className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">Search your favorite cocktail</label>
+          <input type="text" ref={searchValue} onChange={handleValueChange} />
+        </div>
+      </form>
+    </section>
   );
 };
 
